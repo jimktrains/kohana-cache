23115743c894f2d9349200e56995c0dd62056511
Jim Keener <jimktrains@gmail.com>
2010-10-18 11:49:20 -0400

    changed some names

--- classes/session/memcache.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-abstract class Session_Memcache extends Kohana_Session_Memcache {}
---
+++ classes/session/memcached.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Session_Memcached extends Kohana_Session_Memcached {}
